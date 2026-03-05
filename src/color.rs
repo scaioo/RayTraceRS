037a2e90723865024eeb89398377ea53651029d1
scaioo <andrea.scaioli@gmail.com>
2026-03-06 00:08:10 +0100

    style: format the code and adjust attributes

--- src/color.rs
+++ src/color.rs
@@ -1,17 +1,17 @@
 use std::ops::Add;
 
-#[derive(Debug)]
-pub struct Color{
+#[derive(Copy, Clone, Debug)]
+pub struct Color {
     pub r: f32,
     pub g: f32,
     pub b: f32,
 }
 
 // Implementation of the sum (+) for type Color
-impl Add for Color{
+impl Add for Color {
     type Output = Color;
-    fn add(self, rhs: Color) -> Self::Output{
-        Color{
+    fn add(self, rhs: Color) -> Self::Output {
+        Color {
             r: self.r + rhs.r,
             g: self.g + rhs.g,
             b: self.b + rhs.b,
@@ -21,18 +21,30 @@ impl Add for Color{
 
 // Test Add implementation
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
     fn test_add() {
-        let c1: Color = Color { r: 1.0, g: 2.0, b: 3.0};
-        let c2: Color = Color { r: 4.0, g: 5.0, b: 6.0};
-        let c3: Color = Color { r: 5.0, g: 7.0, b: 9.0};
+        let c1: Color = Color {
+            r: 1.0,
+            g: 2.0,
+            b: 3.0,
+        };
+        let c2: Color = Color {
+            r: 4.0,
+            g: 5.0,
+            b: 6.0,
+        };
+        let c3: Color = Color {
+            r: 5.0,
+            g: 7.0,
+            b: 9.0,
+        };
         let sum: Color = c1 + c2;
 
-        assert_eq!(sum.r,c3.r);
-        assert_eq!(sum.g,c3.g);
-        assert_eq!(sum.b,c3.b);
+        assert_eq!(sum.r, c3.r);
+        assert_eq!(sum.g, c3.g);
+        assert_eq!(sum.b, c3.b);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-use rstrace::color as color;
+use rstrace::color;
 fn main() {
     // Leave two lines between the execution and the printing of the main
-    println!{"\n------------------------------------------------------\n"};
+    println! {"\n------------------------------------------------------\n"};
 
     // Implement the first color
-    let our_color: color::Color = color::Color { r: 1.0, g: 2.0, b: 3.0};
-    println!{"Hey! Here's our fist color: {:?} \n", our_color };
+    let our_color: color::Color = color::Color {
+        r: 1.0,
+        g: 2.0,
+        b: 3.0,
+    };
+    println! {"Hey! Here's our fist color: {:?} \n", our_color };
 
     // Implement the second color
-    let another_color: color::Color = color::Color {r: 4.0,g: 5.0, b: 6.0};
-    println!{"Hey! Here's another color : {:?} \n", another_color };
+    let another_color: color::Color = color::Color {
+        r: 4.0,
+        g: 5.0,
+        b: 6.0,
+    };
+    println! {"Hey! Here's another color : {:?} \n", another_color };
 
     // Sum the color!
     let sum: color::Color = our_color + another_color;
-    println!{"Hey! Here the sum!?!?     : {:?} \n", sum };
-
+    println! {"Hey! Here the sum!?!?     : {:?} \n", sum };
 }

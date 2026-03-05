746f8ce067ff8e400b9190ea15fb96ff7ab71f01
scaioo <andrea.scaioli@gmail.com>
2026-03-05 23:34:48 +0100

    feaf and test: Introduced color and add function (+)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-/*=============================================================
-                          Pacchetti  
-=============================================================*/
-// use std::{cmp::Ordering, io}; Per aggiungere più lib da std
+use rstrace::color as color;
+fn main() {
+    // Leave two lines between the execution and the printing of the main
+    println!{"\n------------------------------------------------------\n"};
 
-//             NB: QUESTO FORMAT È DI PROVA!!!
-//                Poi dite se non vi piace. 
+    // Implement the first color
+    let our_color: color::Color = color::Color { r: 1.0, g: 2.0, b: 3.0};
+    println!{"Hey! Here's our fist color: {:?} \n", our_color };
 
-/*=============================================================
-                            Main  
-=============================================================*/
-fn main(){
-    println!("Hello, world!");
+    // Implement the second color
+    let another_color: color::Color = color::Color {r: 4.0,g: 5.0, b: 6.0};
+    println!{"Hey! Here's another color : {:?} \n", another_color };
+
+    // Sum the color!
+    let sum: color::Color = our_color + another_color;
+    println!{"Hey! Here the sum!?!?     : {:?} \n", sum };
+
 }

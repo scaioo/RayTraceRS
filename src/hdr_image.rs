8cd9fbdc51794bee39fba2863c416c57982484e7
Forlani <ISACCOFORLANI@GMAIL.COM>
2026-03-10 18:31:31 +0100

    Feat: implement error handling with Result<T,E> for pixel validation - Add anyhow dependency to Cargo.toml. - Refactored check_position() to return Result. - Updated get_pixel and set_pixel to handle new Result type.

--- src/hdr_image.rs
+++ src/hdr_image.rs
@@ -1,5 +1,6 @@
 use crate::color;
 use crate::color::Color;
+use anyhow::{anyhow, Result};
 
 #[derive(Clone, Debug)]
 pub struct HDR{
@@ -8,8 +9,8 @@ pub struct HDR{
     pub pixels : Vec<Color>,
 }
 
-// Implement the full-black image
 impl HDR{
+    // Implement the full-black image
     pub fn new(width : usize, height : usize) -> HDR{
         let pixels = vec![Color { r: 0.0, g: 0.0, b: 0.0 }; width * height];
         HDR {
@@ -18,20 +19,39 @@ impl HDR{
             pixels,
         }
     }
+    
+    /*===============    NOTE    ================
+          CHECK AND DUBLECHECK WITH TOMASI!!!
+                  is it the best way?
+                 better to propagate?
+     ============================================*/
 
     pub fn set_pixel(&mut self, x : usize, y : usize, color : Color){
-        self.check_position(x, y);
-        self.pixels[y * self.width + x] = color;
+        match self.check_position(x, y){
+            Ok(_) => self.pixels[y * self.width + x] = color,
+            Err(_) => panic!("Pixel Error: pixel {},{} out of bound!", x,y)
+        }
     }
 
     pub fn get_pixel(&self, x : usize, y : usize) -> Color{
-        self.check_position(x, y);
-        self.pixels[y * self.width + x]
-    }
+        match self.check_position(x, y){
+            Ok(_) => self.pixels[y * self.width + x],
+            Err(_) => panic!("Pixel Error: pixel {},{} out of bound!", x,y)
+        }
 
-    // This function must be changed after the 11 March 2026 Laboratory!!!!
-    fn check_position(&self, x : usize, y : usize){
-        assert!(x < self.width && y < self.height);
+        /*               AN ALTERNATIVE:
+         in this case fn -> Result<&HDR>: we propagate the Err
+
+        self.check_position(x, y)?;
+        Ok(self.pixels[y * self.width + x])
+         */
+
+    }
+    
+    fn check_position(&self, x : usize, y : usize) -> Result<&HDR>{
+        if x < self.width && y < self.height{
+            Ok(&self)
+        } else { Err(anyhow!("NON-EXISTING PIXEL ({},{})", x, y))}
     }
 }
 
@@ -66,7 +86,6 @@ mod test{
     #[should_panic]
     fn test_check_position(){
         let hdr = HDR::new(10, 55);
-        hdr.check_position(5, 1);
-        hdr.check_position(50, 1);
+        hdr.check_position(11, 2).unwrap();
     }// NOTE : it would be best to integrate this test with Result options!!!!
 }

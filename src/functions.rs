098f4b95add1a20aad9b29e7a73c586932a82ef2
Forlani <ISACCOFORLANI@GMAIL.COM>
2026-03-12 22:19:11 +0100

    refactor: move generics to functions.rs; update check_position syntax; fix docs

---
+++ src/functions.rs
@@ -0,0 +1,19 @@
+fn are_close(x: f32, y: f32) -> bool {
+    let epsilon = 1e-5;
+    (x - y).abs() < epsilon
+}
+
+// test are_close
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn are_close_test() {
+        let x = 0.11111;
+        let y = 0.11112;
+
+        if !are_close(x, y) {
+            panic!("are_close is not working");
+        }
+    }
+}

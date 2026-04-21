f0cc9e77d5d13550ff8e0ca10add87e871fa2419
Forlani <ISACCOFORLANI@GMAIL.COM>
2026-04-21 15:07:18 +0200

    Add display, is_close and at methods

--- src/ray.rs
+++ src/ray.rs
@@ -6,7 +6,10 @@
 //! - `t_min : f32` ... TODO DOCKING
 
 use std::f32;
-use crate::geometry::{Vector, Point};
+use crate::functions::are_close;
+use crate::geometry::{Vector, Point, is_close};
+use anyhow::{Result, anyhow};
+use std::fmt::{Display, Formatter};
 
 
 //================================================================
@@ -23,7 +26,7 @@ pub struct Ray{
 }
 
 //================================================================
-//                          Constructor
+//                         Constructor
 //================================================================
 
 impl Ray {
@@ -37,6 +40,33 @@ impl Ray {
        }
    }
 
+}
+
+//================================================================
+//                         Display
+//================================================================
+
+impl Display for Ray {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Ray:\nO: {},\nD: {},\nt: [{}, {}],\ndepth: {}",
+            self.origin,
+            self.dir,
+            self.t_min,
+            self.t_max,
+            self.depth
+        )
+    }
+}
+
+
+//================================================================
+//                          Methods
+//================================================================
+
+impl Ray {
+
     pub fn set_depth(&mut self, depth : usize) {
         self.depth = depth;
     }
@@ -44,6 +74,23 @@ impl Ray {
     pub fn set_borders(&mut self, t_max: f32, t_min: f32){
         self.t_max = t_max;
         self.t_min = t_min;
+    }
+
+    pub fn is_close(self, other: Ray) -> Result<bool> {
+        let condition = is_close(self.origin, other.origin)
+        && is_close(self.dir, other.dir)
+        && self.t_max == other.t_max
+        && self.t_min == other.t_min
+        && self.depth == other.depth;
+
+        if !condition {
+            Err(anyhow!("Two rays are not approximately equal!\
+            \nFirst ray:\n{}\nSecond ray:\n{}\n", self, other))
+        } else { Ok(true) }
+    }
+
+    pub fn at(&self, t : f32) -> Point {
+        self.origin + t * self.dir
     }
 }
 
@@ -68,6 +115,29 @@ mod tests {
         assert!(ray.t_max.is_infinite());
         assert!(are_close(ray.t_min, 1e-5));
         assert_eq!(ray.depth, 0);
+    }
+
+    #[test]
+    fn test_display(){
+        let origin = Point::new(1.0, 2.0, 3.0);
+        let dir = Vector::new(4.0, 5.0, 6.0);
+        let ray = Ray::new(origin, dir);
+
+        assert_eq!(format!("{}", ray), "Ray:\nO: Point(x = 1, y = 2, z = 3),\n\
+        D: Vector(x = 4, y = 5, z = 6),\nt: [0.00001, inf],\ndepth: 0");
+    }
+
+    #[test]
+    fn test_is_close(){
+        let ray1 = Ray::new(
+            Point::new(1.0, 2.0, 3.0),
+            Vector::new(4.0, 5.0, 6.0)
+        );
+        let ray2 = Ray::new(
+            Point::new(1.0, 1.9999999, 3.0),
+            Vector::new(4.000001, 5.0, 6.0)
+        );
+        ray1.is_close(ray2).unwrap();
     }
 
     #[test]

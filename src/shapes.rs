1fbee76283ba048d523635b90c46d28f5373e48f
Forlani <ISACCOFORLANI@GMAIL.COM>
2026-04-26 16:04:17 +0200

    Complete draft

--- src/shapes.rs
+++ src/shapes.rs
@@ -4,16 +4,21 @@
 //! can put in the image tracer scene. Then follows the shape classes: `Sphere`, `Plane` and `Triangle`.
 //!
 //! All the documentation is a WIP - draft!
+
+use std::ops::Mul;
+use crate::camera::{Camera, PerspectiveCamera};
+use crate::functions::{are_close, transpose_matrix};
 use crate::hit_record::HitRecord;
 use crate::ray::Ray;
-use crate::geometry::{Normal, Point, Vec2D};
+use crate::geometry::{Dot, Normal, Point, Vec2D, Vector};
+use crate::transformations::{IsHomogeneousMatrix, Transformation};
 
 pub trait Shape {
-     fn ray_intersection(&self, ray: &Ray) -> Option<HitRecord>;
+    fn ray_intersection(&self, ray: Ray) -> Option<HitRecord>;
 
     fn normal_at(&self) -> Normal;
 
-   fn point_to_uv(&self, point: &Point) -> Vec2D;
+    fn point_to_uv(&self, point: &Point) -> Vec2D;
 }
 
 /// The class Sphere adds the possibility to represent spherical objects in images
@@ -30,7 +35,77 @@ pub trait Shape {
 /// All of this is for the unit sphere. To obtain other pseudo-spherical objects
 /// we use transformations.
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Sphere{}
+pub struct Sphere<T: IsHomogeneousMatrix>{
+    pub transformation: T // Is it where the sphere is at?
+}
+
+impl<T: IsHomogeneousMatrix> Sphere<T> {
+    pub fn new(transformation: T) -> Self {
+        Self { transformation }
+    }
+}
+
+impl<T> Shape for Sphere<T>
+where
+    T: IsHomogeneousMatrix
+    + Mul<Ray, Output = Ray>
+    + Copy
+    + Mul<Point, Output = Point>
+    + Mul<Normal, Output = Normal>
+    + Mul<Vector, Output = Vector>
+{
+    fn ray_intersection(&self, ray: Ray) -> Option<HitRecord> {
+        let inverse_transformation =
+            self.transformation.inverse_transformation();
+        let transformed_ray = inverse_transformation * ray;
+
+        let origin =
+            transformed_ray.origin - Point::new(0.0, 0.0, 0.0);
+
+        let module_of_d = transformed_ray.dir.norm();
+        let o_times_d =  origin.dot(&transformed_ray.dir);
+        let delta_fourth : f32 =
+            o_times_d * o_times_d - module_of_d * module_of_d * (origin.norm() - 1.0);
+
+        if delta_fourth < 0.0 || are_close(delta_fourth, 0.0) {
+            return None;
+        } else {
+            let t_1 : f32 = (- o_times_d - delta_fourth.sqrt()) / module_of_d;
+            let t_2 : f32 = (- o_times_d + delta_fourth.sqrt()) / module_of_d;
+
+            let condition = |t:f32| t > transformed_ray.t_min && t < transformed_ray.t_max;
+            if condition(t_1){
+                let hit_point = transformed_ray.at(t_1);
+                Some( HitRecord {
+                    world_point : self.transformation * hit_point,
+                    normal : self.transformation * self.normal_at(),
+                    surface_normal : self.point_to_uv(&hit_point),
+                    t : t_1,
+                    ray
+                })
+            }else if condition(t_2){
+                let hit_point = transformed_ray.at(t_2);
+                Some(HitRecord{
+                    world_point : self.transformation * hit_point,
+                    normal : self.transformation * self.normal_at(),
+                    surface_normal : self.point_to_uv(&hit_point),
+                    t : t_2,
+                    ray
+                })
+            } else { None } // Does it make sense?
+        }
+    }
+
+    fn normal_at(&self) -> Normal {
+        // TODO!!!!!
+        Normal::new(0.0,0.0,0.0)
+    }
+
+    fn point_to_uv(&self, point: &Point) -> Vec2D {
+        // TODO!!!!!
+        Vec2D::new(point.x, point.y)
+    }
+}
 
 /// The class Plane adds the possibility to represent the plane in an image
 ///

e9e56d86d79fac9d9a51a66b1f28ef712042afd6
Forlani <ISACCOFORLANI@GMAIL.COM>
2026-05-16 12:06:05 +0200

    Implement UniformPigment and unit tests

--- src/pigments.rs
+++ src/pigments.rs
@@ -7,18 +7,61 @@
 
 use crate::color::Color;
 use crate::geometry::Vec2D;
-use crate::ray;
 
 
 // ===============================================
 // Pigment type
 // ==============================================
 
-/// This is the marker trait for `Pigment` types. 
+/// This is the marker trait for `Pigment` types.
 pub trait Pigment {
     /// Returns the `Color` of a certain point on the surface.
     fn get_color(&self, uv: &Vec2D) -> Color;
 }
 
+// ===============================================
+// UniformPigment
+// ==============================================
 
+/// The surface has only one color!
+pub struct UniformPigment{
+    pub color : Color,
+}
+
+impl UniformPigment {
+    pub fn new(color : Color) -> Self {
+        UniformPigment { color }
+    }
+}
+
+impl Pigment for UniformPigment {
+    fn get_color(&self, _uv: &Vec2D) -> Color {
+        self.color
+    }
+}
+
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_uniform_pigment_constructor() {
+        let color = Color::new(1.0, 2.0, 3.0);
+        let pigment = UniformPigment::new(color);
+        assert_eq!(color, pigment.color);
+    }
+
+    #[test]
+    fn test_uniform_pigment_pigments() {
+        let color = Color::new(1.0, 2.0, 3.0);
+        let pigment = UniformPigment::new(color);
+        assert_eq!(pigment.get_color(&Vec2D { x: 0.0, y: 0.0 }), color);
+    }
+}
 
